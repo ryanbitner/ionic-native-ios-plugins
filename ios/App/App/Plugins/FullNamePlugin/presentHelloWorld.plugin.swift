a1ae22848e2a32804d2e5be40c7deeb65d5c8460
ryanbitner <131702725+ryanbitner@users.noreply.github.com>
2023-05-02 14:23:56 -0600

    Update Present world
    - Added changes to present world for input.

--- ios/App/App/Plugins/FullNamePlugin/presentHelloWorld.plugin.swift
+++ ios/App/App/Plugins/FullNamePlugin/presentHelloWorld.plugin.swift
@@ -1,23 +1,65 @@
 import Foundation
 import Capacitor
+import SwiftUI
 
-@objc(PresentHelloWorldPlugin)
-public class PresentHelloWorldPlugin: CAPPlugin {
-    private let implementation = PresentHelloWorld()
+@objc(FullNamePlugin)
+public class FullNamePlugin: CAPPlugin {
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
         call.resolve([
-            "value": implementation.echo(value)
+            "value": "hi"
         ])
     }
 
-    @obj func presentView(_ call: CAPPluginCall) {
+    @objc func getFullName(_ call: CAPPluginCall) {
+        let firstName = call.getString("firstName") ?? ""
+        let lastName = call.getString("lastName") ?? ""
+
+        call.resolve([
+            "value": "\(firstName) \(lastName)"
+        ])
+    }
+    
+    @objc func presentHelloWorld(_ call: CAPPluginCall) {
+        let fullName = call.getString("fullName") ?? ""
+        
         guard let bridge = self.bridge else {return}
-        let customView = HelloWorldView()
         DispatchQueue.main.async {
-            self.bridge?.viewController.present(customView, animated: true, completion: nil)
+            let viewController = UIHostingController(rootView: HelloWorldView(fullName: fullName, call: call));
+            self.bridge?.viewController?.present(viewController, animated: true, completion: nil)
+            
         }
-        call.resolve
+        
+        
+    }
+}
+
+struct HelloWorldView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    var fullName: String
+    var call: CAPPluginCall
+    
+    func onSubmit() -> Void {
+        call.resolve([
+            "value": textEntered,
+        ])
+        self.presentationMode.wrappedValue.dismiss()
+    }
+    
+    @State private var textEntered: String = ""
+    
+    var body: some View {
+        Text("Hello \(fullName)")
+        
+        TextField(
+            "Enter Text",
+            text: $textEntered
+        )
+        
+        Button("Submit", action: onSubmit)
+        
+        
     }
 }
