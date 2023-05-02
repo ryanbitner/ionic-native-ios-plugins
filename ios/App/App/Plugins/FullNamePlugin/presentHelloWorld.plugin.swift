7b14763e5534e4c78084ea89ca0eb1681cf2c7a6
ryanbitner <131702725+ryanbitner@users.noreply.github.com>
2023-05-02 14:01:44 -0600

    Update Plugin
    Added input on the view.

--- ios/App/App/Plugins/FullNamePlugin/presentHelloWorld.plugin.swift
+++ ios/App/App/Plugins/FullNamePlugin/presentHelloWorld.plugin.swift
@@ -1,20 +1,23 @@
 import Foundation
 import Capacitor
-import SwiftUI
 
-@obj func presentView(_ call: CAPPluginCall) {
+@objc(PresentHelloWorldPlugin)
+public class PresentHelloWorldPlugin: CAPPlugin {
+    private let implementation = PresentHelloWorld()
+
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": implementation.echo(value)
+        ])
+    }
+
+    @obj func presentView(_ call: CAPPluginCall) {
         guard let bridge = self.bridge else {return}
         let customView = HelloWorldView()
         DispatchQueue.main.async {
             self.bridge?.viewController.present(customView, animated: true, completion: nil)
         }
         call.resolve
-    }
-
-
-
-struct HelloWorldView: View {
-    var body: some View {
-        Text("Hello World")
     }
 }
